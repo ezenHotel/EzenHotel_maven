b11702617b2730e96a022d90d15185b8ba54028b
j-i-i-n <gamja941126@gmail.com>
2022-10-20 16:33:43 +0900

    def set

--- Proj_EHotel_Maven/src/main/java/pack/spring/basic/MemberController.java
+++ Proj_EHotel_Maven/src/main/java/pack/spring/basic/MemberController.java
@@ -1,8 +1,12 @@
 package pack.spring.basic;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,4 +15,33 @@ public class MemberController {
 	public ModelAndView create() {
 		return new ModelAndView("member/create");
 	}
+
+	@Autowired
+	MemberService memberService;
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ModelAndView createPost(@RequestParam Map<String, Object> map) {
+		ModelAndView mav = new ModelAndView();
+		String memberId = this.memberService.create(map);
+		if (memberId == null) {
+			mav.setViewName("redirect:/create");
+		} else {
+			mav.setViewName("redirect:/detail?memberId=" + memberId);
+		}
+		return mav;
+	}
+
+	// 상세보기 시작
+	@RequestMapping(value = "/detail", method = RequestMethod.GET)
+	public ModelAndView detail(@RequestParam Map<String, Object> map) {
+		Map<String, Object> detailMap = this.memberService.detail(map);
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("data", detailMap);
+		String no = map.get("no").toString();
+		mav.addObject("no", no);
+		mav.setViewName("/member/detail");
+		return mav;
+	}
+	// 상세보기 끝
+
 }
---
+++ Proj_EHotel_Maven/src/main/java/pack/spring/basic/MemberService.java
@@ -0,0 +1,11 @@
+package pack.spring.basic;
+
+import java.util.Map;
+
+public interface MemberService {
+	
+	String create(Map<String, Object> map);
+	
+	Map<String, Object> detail(Map<String, Object> map);
+	
+}
